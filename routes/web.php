81bbaca76485d4f792d080a177cb60eae4d1dfb0
jogeocc <jogeochan@gmail.com>
2022-10-11 16:59:53 -0500

    1

--- routes/web.php
+++ routes/web.php
@@ -93,3 +93,7 @@ Route::prefix('admin')->group(function () {
 });
 
 Route::get('editar_cotizacion_prenda/{id}', [CotizacionPrendaController::class, 'edit'])->name('cotizacionprenda.edit');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
