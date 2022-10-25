16b8a516ca8da849e0bb6a49a1da867788f74bbd
jogeocc <jogeochan@gmail.com>
2022-10-24 19:17:37 -0500

    cambios-2022

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use App\Models\TicketsDesempeño;
+use App\Models\Prenda;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\View;
 
@@ -69,6 +70,91 @@ class TicketController extends Controller
         $ticket = TicketsDesempeño::make($request->all());
         $ticket->save();
 
+    /*     //prueba
+        $prenda=new Prenda();
+  
+        $prenda->folio_cotizacion=0;
+        $prenda->nombre_prenda=0;
+        $prenda->descripcion_generica=0;
+        $prenda->kilataje_prenda=0;
+        $prenda->gramaje_prenda=0;
+        $prenda->caracteristicas_prenda=0;
+        $prenda->avaluo_prenda=0;
+        $prenda->porcentaje_prestamo_sobre_avaluo=0;
+        $prenda->prestamo_inicial=0;
+        $prenda->prestamo_prenda=0;
+        $prenda->fecha_prestamo=0;
+        $prenda->fecha_comercializacion='24-10-22';
+        $prenda->mes1='24-10-22';
+        $prenda->interes=0;
+        $prenda->almacenaje=0;
+        $prenda->iva=0;
+        $prenda->refrendo=0;
+        $prenda->desempeño=0;
+        $prenda->subtotal=0;
+        $prenda->mes2='24-10-22';
+        $prenda->interes2=0;
+        $prenda->almacenaje2=0;
+        $prenda->iva2=0;
+        $prenda->refrendo2=0;
+        $prenda->desempeño2=0;
+        $prenda->subtotal2=0;
+        $prenda->mes3='24-10-22';
+        $prenda->interes3=0;
+        $prenda->almacenaje3=0;
+        $prenda->iva3=0;
+        $prenda->refrendo3=0;
+        $prenda->desempeño3=0;
+        $prenda->subtotal3=0;
+        $prenda->mes4='24-10-22';
+        $prenda->interes4=0;
+        $prenda->almacenaje4=0;
+        $prenda->iva4=0;
+        $prenda->refrendo4=0;
+        $prenda->desempeño4=0;
+        $prenda->subtotal4=0;
+        $prenda->mes5='24-10-22';
+        $prenda->interes5=0;
+        $prenda->almacenaje5=0;
+        $prenda->iva5=0;
+        $prenda->refrendo5=0;
+        $prenda->desempeño5=0;
+        $prenda->subtotal5=0;
+        $prenda->abono_capital=0;
+        $prenda->importe_anterior=0;
+        $prenda->importe_actual=0;
+        $prenda->interes_anterior=0;
+        $prenda->almacenaje_anterior=0;
+        $prenda->iva_anterior=0;
+        $prenda->refrendo_anterior=0;
+        $prenda->numeros_refrendos=0;
+        $prenda->cantidad_pago=0;
+        $prenda->cambio_boleta=0;
+        $prenda->folio_refrendo=0;
+        $prenda->sub_refrendo=0;
+        $prenda->total=0;
+        $prenda->recargo_des=0;
+        $prenda->hora_refrendo='24-10-22';
+        $prenda->folio_capi=0;
+        $prenda->abono_capital_capi=0;
+        $prenda->interes_anterior_capi=0;
+        $prenda->almacenaje_anterior_capi=0;
+        $prenda->sub_capital=11111;
+        $prenda->iva_anterior_capi=0;
+        $prenda->total_capi=0;
+        $prenda->cantidad_pago_capi=0;
+        $prenda->cambio_boleta_capi=0;
+        $prenda->importe_anterior_capi=0;
+        $prenda->importe_actual_capi=0;
+        $prenda->recargo_des_capi=0;
+        $prenda->numeros_capital=0;
+        $prenda->cantidad_prenda=0;
+        $prenda->id_cliente=1;
+        
+        $prenda->save(); */
+        ///fin prueba
+     DB::table('prendas')->where('id_prendas',$ticket->id_prendas)->delete();
+
         return redirect()->route('Ticket_Desempeño')->with('registro','pago');
     }
 
@@ -83,4 +169,6 @@ class TicketController extends Controller
             ]
         );
     }
+ 
+
 }
