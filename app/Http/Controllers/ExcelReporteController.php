102ca72990246b8fd1e95b9b3bac6897f90aaf5f
jogeocc <jogeochan@gmail.com>
2022-10-26 12:11:15 -0500

    Cambios 2610221211

--- app/Http/Controllers/ExcelReporteController.php
+++ app/Http/Controllers/ExcelReporteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Cliente; // PARA USAR LA TABLA CLIENTES
+use App\Models\desempeños;// PARA USAR LA TABLA DESEMPEÑOS
 
 
 class ExcelReporteController extends Controller
@@ -60,4 +61,56 @@ class ExcelReporteController extends Controller
        //Esto hace que Laravel exponga el archivo como descarga
        return response()->stream($callback, 200, $headers);
     }
+    public function exportDesempeños(){
+
+        //Nombre del archivo que generaremos
+        $fileName = 'ReporteDesempeños.csv';
+        //Arreglo que contendrá las filas de datos
+        $arrayDetalle = Array();
+ 
+        //Estos son los datos que recibimos del modelo que queremos leer, aquí ustedes cámbienlo por el modelo que necesiten
+        $items=desempeños::all();
+ 
+        //El encabezado que le dice al explorador el tipo de archivo que estamos generando
+        $headers = array(
+                    "Content-type"        => "text/csv",
+                    "Content-Disposition" => "attachment; filename=$fileName",
+                    "Pragma"              => "no-cache",
+                    "Cache-Control"       => "must-revalidate, post-check=0, pre-check=0",
+                    "Expires"             => "0"
+ 
+                    );
+ 
+        $columns = array(
+             'Fecha',
+             'Folio_Boleta',
+             'avaluo_prenda',
+             'telefono_cliente',
+             'porcentaje_prestamo_sobre_avaluo',
+             'NumSocio');
+ 
+        foreach ($items as $item){
+            $arrayDetalle[] = array(
+                             'created_at' => $item->created_at,
+                             'id_prendas' => $item->id_prendas,
+                             'avaluo_prenda' =>$item->avaluo_prenda,
+                             'porcentaje_prestamo_sobre_avaluo' => $item->porcentaje_prestamo_sobre_avaluo,
+                             'prestamo_inicial'  => $item->prestamo_inicial,                                       
+                             'NumSocio'  => $item->numero_socio
+                                );
+        }
+ 
+        $callback = function() use($arrayDetalle, $columns) {
+            $file = fopen('php://output', 'w');
+            //si no quieren que el csv muestre el titulo de columnas omitan la siguiente línea.
+            fputcsv($file, $columns);
+                  foreach ($arrayDetalle as $item) {
+                      fputcsv($file, $item);
+                  }
+                  fclose($file);
+              };
+      
+        //Esto hace que Laravel exponga el archivo como descarga
+        return response()->stream($callback, 200, $headers);
+     }
 }
