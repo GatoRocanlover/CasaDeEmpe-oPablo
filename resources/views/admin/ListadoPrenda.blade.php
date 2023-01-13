8f9d64f835930721535ab5c02609bf9b971d2a42
jogeocc <jogeochan@gmail.com>
2023-01-13 12:38:27 -0600

    1

--- resources/views/admin/ListadoPrenda.blade.php
+++ resources/views/admin/ListadoPrenda.blade.php
@@ -140,14 +140,17 @@
                                 </tr>
                             @endforeach
                         </table>
-
-                        {!! $lista_prendas->links() !!}
-
+                        <div class="d-flex  justify-content-end">
+                            {!! $lista_prendas->links() !!}
+                        </div>
                     </div>
+
                 @else
                     <div class="h3 text-center fw-bold mt-8">No tienes los permisos para ver este modulo <br> Comunicate con
                         tu superior...</div>
                 @endcan
+ 
+                
 </body>
 
 
